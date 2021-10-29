8501b6aaf22b6304e7266fecd1d8359fcd853163
YounJ00 <hamyounjin006@gmail.com>
2021-10-29 22:57:20 +0900

    commits

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    const style1 = {
+      backgroundColor: 'black',
+      padding: '16px',
+      color: 'white',
+      fontSize: '12px'
+    };
+
+    return(
+      <div style={style1}>
+        hi there
+     </div>
+    )
+  }
 }
+
+/**
+ class App extends Component {
+   render(){
+     return(
+       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+     )
+   }
+ }
+ */
 
 export default App;
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//브라우저 상에 작성한 리액트 컴포넌트를 보여주기 위해서는 ReactDOM.render()함수를 사용함
+//첫번째 파라미터는 렌더링 할 결과물이고, 두번째에는 컴포넌트를 어떤 DOM에 그릴지 정해준다.
 ReactDOM.render(
   <React.StrictMode>
     <App />
