5f4a8515cb85e0ca3820aeb7fdee73d4bc90d4ae
YounJ00 <hamyounjin006@gmail.com>
2021-11-04 23:58:15 +0900

    LifeCycle API

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -5,21 +5,49 @@ class Counter extends Component {
         number : 0
     }
 
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount (deprecated)');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        //5의 배수라면 리렌더링 하지 않음
+        console.log('shouldComponentUpdate');
+        if(nextState.number % 5 == 0) return false;
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('componentWillUpdate');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate');
+    }
+
     handleIncrease = () => {
         const {number} = this.state;
         this.setState({
-            number : number + 1
+            number: number + 1
         });
     }
 
     handleDecrease = () => {
         this.setState(
             ({number}) => ({
-                number : number - 1
+                number: number - 1
             })
         );
     }
-
+    
     render() {
         return (
             <div>
