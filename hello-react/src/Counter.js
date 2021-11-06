d23b3b42c683b415e289d8ab37c326e0ee45290c
YounJ00 <hamyounjin006@gmail.com>
2021-11-06 20:06:04 +0900

    componentDidCatch

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,8 +1,18 @@
 import React, {Component} from "react";
 
+const Problematic = () => {
+    throw (new Error('버그가 나타났다!'));
+    return (
+        <div>
+
+        </div>
+    );
+};
+
 class Counter extends Component {
     state = {
-        number : 0
+        number : 0,
+        error: false
     }
 
     constructor(props) {
@@ -21,7 +31,7 @@ class Counter extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         //5의 배수라면 리렌더링 하지 않음
         console.log('shouldComponentUpdate');
-        if(nextState.number % 5 == 0) return false;
+        if(nextState.number % 5 === 0) return false;
         return true;
     }
 
@@ -47,12 +57,21 @@ class Counter extends Component {
             })
         );
     }
+
+    componentDidCatch(error, info) {
+        this.setState({
+            error: true
+        });
+    }
     
     render() {
+        if(this.state.error) return (<h1>에러발생!</h1>);
+
         return (
             <div>
                 <h1>카운터</h1>
                 <div>값: {this.state.number}</div>
+                { this.state.number === 4 && <Problematic /> }
                 <button onClick={this.handleIncrease}>+</button>
                 <button onClick={this.handleDecrease}>-</button>
             </div>
