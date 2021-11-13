2f73ee0cecad852f2c4d691e868a477da960c88b
YounJ00 <hamyounjin006@gmail.com>
2021-11-13 23:11:28 +0900

    array-concat,map

--- phone-book/src/components/PhoneInfo.js
+++ phone-book/src/components/PhoneInfo.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 class PhoneInfo extends Component {
+    //undefined시를 고려해서 defaultProps를 통해 info의 기본값을 설정해준다.
     static defaultProps = {
+        //info객체 생성(props로 보낼 것이다.)
         info: {
             name: '이름',
             phone: '010-0000-0000',
-            id: 0
+            id: 0 //배열 생성시에 고유값을 key로 설정하기 위해 id를 선언해준다.
         }
     }
 
--- phone-book/src/components/PhoneInfoList.js
+++ phone-book/src/components/PhoneInfoList.js
@@ -3,14 +3,16 @@ import PhoneInfo from './PhoneInfo';
 
 class PhoneInfoList extends Component {
     static defaultsProps = {
-        data: []
+        data: [] //data 배열 생성
     }
 
     render() {
         const {data} = this.props;
         const list = data.map(
             info => (<PhoneInfo key={info.id} info={info}/>)
-        );
+            //key는 리액트에서 배열을 렌더링할 때 꼭 필요한 값이다.
+            //만약에 key값을 부여하지 않으면, 배열의 index값이 자동으로 key로 설정된다.(비효율적임)
+        ); //map을 통해 JSX로 변환해준다.
 
         return (
             <div>
