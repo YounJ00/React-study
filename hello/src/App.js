88ed6ee681604059653386d30b471e109ea372a1
YounJ00 <hamyounjin006@gmail.com>
2022-01-21 23:41:51 +0900

    hello

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,13 +1,16 @@
 import React, {Component} from "react";
 
+//클래스를 통해 컴포넌트를 만들어준다.
 class App extends Component {
   state = {
     hello : 'hello app js!'
   };
 
+  //클래스 형태로 만들어진 컴포넌트에는 꼭, render()함수가 있어야 한다.
   render() {
+    //render()함수 안에는 JSX(html코드)를 return해줘야한다.
     return <div className="App">{this.state.hello}</div>;
   }
 }
 
-export default App;
+export default App; //작성한 컴포넌트를 다른 곳에서 불러와서 사용할 수 있도록 내보내기를 해준다.
--- hello/src/index.js
+++ hello/src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//id가 root인 DOM을 찾아서 App을 렌더링해라.
+//root DOM은 public/index.html 파일에서 찾아볼 수 있다.
+ReactDOM.render(<App />, document.getElementById('root')); 
